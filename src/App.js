8b127067f41babd24359c6f1c4aa27f5eb3c344b
mathurahravigulan <mathurah.ravigulan@edu.uwaterloo.ca>
2020-04-29 10:08:24 -0400

    fixes

--- src/App.js
+++ src/App.js
@@ -1,51 +1,57 @@
-import React,{ Component } from 'react';
-import './App.css';
-import Learned from './learned';
-  
-require('dotenv').config()
+import React, { Component } from 'react'
+import './App.css'
+import orderBy from 'lodash/orderBy'
+import Learned from './learned'
 
 class App extends Component {
-  constructor(props){
-    super(props);
+  constructor(props) {
+    super(props)
+
     this.state = {
       learning: [],
-    };
+    }
   }
 
   componentDidMount() {
-
-
-    fetch('https://api.airtable.com/v0/appjbN2ful2c6dacW/Imported%20table?api_key=keyM53ymgfikEXuqb')
-    
-    .then((resp) => resp.json())
-    .then(data => {
-       this.setState({ learning: data.records });
-    }).catch(err => {
-      // Error :(
-    });
-
+    this.initData()
   }
 
-  
-  
+  initData() {
+    fetch('https://api.airtable.com/v0/appk0O7TZ7UxoYsSG/Table%201?api_key=keyM53ymgfikEXuqb')
+      .then(resp => resp.json())
+      .then(data => {
+        const formattedData = data.records.map((o, index) => ({
+          ...o.fields,
+          index,
+          id: o.id,
+          key: o.id,
+        }))
+
+        this.setState({
+          learning: orderBy(formattedData, ['date'], ['asc']),
+        })
+      })
+  }
+
   render() {
     return (
       <div className="container mt-5">
         <div className="row">
-            <div className="col">
+          <div className="col">
             <h1>Date</h1>
-            </div>
-            <div className="col">
-            <h1>What I learned</h1>
-            </div>
           </div>
-          <div>
-            {this.state.learning.map(learnings=> <Learned {...learnings.fields}/>)}
-            </div>
+          <div className="col">
+            <h1>Learnings</h1>
           </div>
-    );
+        </div>
+        <div>
+          {this.state.learning.map(learnings => (
+            <Learned {...learnings} />
+          ))}
+        </div>
+      </div>
+    )
   }
 }
 
-
-export default App;
+export default App
